4790542ad260721e65470d0409fdbbfa0ae2e162
Divyabala10 <113165631+Divyabala10@users.noreply.github.com>
2023-01-27 15:55:09 +0530

    Add files via upload

---
+++ inheritance/src/com/Divyabala/packages/Testclass.java
@@ -0,0 +1,20 @@
+package com.divyabala.packages;
+
+
+public class Testclass {
+	
+	
+	
+	
+	
+	
+
+        public static void main(String[] args) {
+            Hlo h = new Hlo();
+            h.design();
+        }
+
+
+
+
+}
